9461c392209e2e4df148abdef6f98d5ff27863fd
1rebecca1 <rebekabekele36@gmail.com>
2022-04-07 21:44:13 +0300

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * Description:main - entery point
  *
  * return:0 after entering the function
- * /
+ */
  int main(void)
  puts("\"Programming is like building a multilingual puzzle");
  return(0);
