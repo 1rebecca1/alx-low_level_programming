0f4ee5e5c8f3eba1867ef057840790af70f33675
1rebecca1 <rebekabekele36@gmail.com>
2022-04-10 12:24:40 +0300

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char lc alphabet
+for (lc alphabet = 'a'; lc alphabet <= 'z'; lc alphabet++)
+{
+putchar(lc alphabet);
+}
+putchar('\n');
+return (0);
+}
