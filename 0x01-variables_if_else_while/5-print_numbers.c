2121c691800c76cdf441dc9035eaee2d437e3972
1rebecca1 <rebekabekele36@gmail.com>
2022-04-10 14:59:25 +0300

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+{
+printf("%number", number);
+}
+printf('\n');
+return (0);
+}
