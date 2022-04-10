0ec781331f13b71541e6283ae935378903cc881a
1rebecca1 <rebekabekele36@gmail.com>
2022-04-10 14:31:20 +0300

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-char uppercase;
 char lowercase;
-for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-{
-putchar(uppercase);
-}
+char uppercase;
 for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 {
 putchar(lowercase);
+}
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
+putchar(uppercase);
 }
 putchar ('\n');
 return (0);
