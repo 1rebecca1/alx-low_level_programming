948affbb4a120fe187a84e52fad481951f8a9cae
1rebecca1 <rebekabekele36@gmail.com>
2022-04-10 20:34:41 +0300

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-int numb;
-for (numb = 0;  numb < 10; numb++)
+int num;
+for (num = 0;  num < 10; num++)
 	putchar((num % 10) + '0');
 {
-putchar (numb);
+putchar (num);
 }
 putchar ('\n');
 return (0);
