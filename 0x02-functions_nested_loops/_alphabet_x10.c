0b241c07213808d330c5d86fba1c99fa13940558
1rebecca1 <rebekabekele36@gmail.com>
2022-04-14 00:10:32 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet
+ * Retern Always 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+for (alphabet = 'a'; alphabet < = 'z');
+_putchar(alphabet)
+_putchar('\n');
+
+return (0);
+	
