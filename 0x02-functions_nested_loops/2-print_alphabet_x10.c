aa28523fd4c0ff27cb715c8fa5a9114872833a54
1rebecca1 <rebekabekele36@gmail.com>
2022-04-16 21:54:44 +0300

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,9 @@ void print_alphabet_10times(void)
 	int count = 0;
 	char alphabet;
 	while (count++ <= 9)
+{
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	_putchar(alphabet);
 	_putchar('\n');
+}
 }
