ab9ebdf31a31a900b75cbb57d738cbdd1ff246b1
1rebecca1 <rebekabekele36@gmail.com>
2022-04-16 13:51:49 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,12 @@
  * print_alphabet
  * Return: Always 0.
  */
-
-void print_alphabet(void)
 {
+void print_alphabet(void)
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
 
 _putchar('\n');
-return(0);
 }
