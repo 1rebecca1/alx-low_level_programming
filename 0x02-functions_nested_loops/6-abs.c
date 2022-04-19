788616f9453eecb679e607d58d947aacdde429e1
1rebecca1 <rebekabekele36@gmail.com>
2022-04-19 22:01:02 +0300

    abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print: computes the absolute value of an integer.
+ * 
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+return (ab * ((ab > 0) - (ab < 0)));
+}
